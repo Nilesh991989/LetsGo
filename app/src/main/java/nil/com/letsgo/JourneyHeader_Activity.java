2cb53137ea36b4d1ab2d44515cc03781d52dd59c
unknown <nileshdoshi991989@gmail.com>
2018-09-08 10:54:37 +0530

    adding add place activity

--- app/src/main/java/nil/com/letsgo/JourneyHeader_Activity.java
+++ app/src/main/java/nil/com/letsgo/JourneyHeader_Activity.java
@@ -4,19 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class JourneyHeader_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_journey_header);
     }
 
-    public void startJourney(View view){
-        Button button = findViewById(view.getId());
-        Intent i = new Intent(getApplicationContext(),JourneyHeader_Activity.class);
+    public void redirectToPlace(View view){
+        Intent i = new Intent(getApplicationContext(),AddPlace_Activity.class);
         startActivity(i);
     }
 }
